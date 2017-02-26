5e33ca62d682e9786aefcdf9d20b6b2d8a9e9467
Chirag <hotandsmart4@gmail.com>
2017-02-27 01:41:53 +0530

    Signed-off-by: Chirag <hotandsmart4@gmail.com>

--- GitProject/src/com/test/git/GitClass.java
+++ GitProject/src/com/test/git/GitClass.java
@@ -13,7 +13,7 @@ public class GitClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Chirag Rocks!!");
+		System.out.println("Chirag Rocks with Git!!");
 
 	}
 
