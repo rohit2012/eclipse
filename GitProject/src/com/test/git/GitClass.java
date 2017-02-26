b24000d7d660276c63bb58681022b6fc1b129f4b
Chirag <hotandsmart4@gmail.com>
2017-02-27 01:35:24 +0530

    Signed-off-by: Chirag <hotandsmart4@gmail.com>

---
+++ GitProject/src/com/test/git/GitClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.git;
+
+/**
+ * @author CHIRAG
+ *
+ */
+public class GitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Chirag Rocks!!");
+
+	}
+
+}
